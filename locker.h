25c05fc24579b6c39f3f9448bb5bcb7e185519e2
poi-t <2273304740@qq.com>
2021-04-30 23:34:07 +0800

    线程池类

--- locker.h
+++ locker.h
@@ -1,13 +1,13 @@
 #ifndef LOCKER_H
 #define LOCKER_H
 
-#include <exception>
 #include <pthread.h>
+#include <exception>
 #include <semaphore.h>
 
-// 线程同步机制封装类
+//线程同步机制封装类
 
-// 互斥锁类
+//互斥锁类
 class locker {
 public:
     locker() {
@@ -31,37 +31,34 @@ public:
     pthread_mutex_t* get() {
         return &m_mutex;
     }
-
 private:
     pthread_mutex_t m_mutex;
 };
 
 
-// 条件变量类
+//条件变量类
 class cond {
 public:
     cond() {
-        if (pthread_cond_init(&m_cond, NULL) != 0) {
+        if(pthread_cond_init(&m_cond, NULL) != 0) {
             throw std::exception();
         }
     }
+
     ~cond() {
         pthread_cond_destroy(&m_cond);
     }
 
-    bool wait(pthread_mutex_t *m_mutex) {
-        return pthread_cond_wait(&m_cond, m_mutex) == 0
+    bool wait(pthread_mutex_t* m_mutex) {
+        return pthread_cond_wait(&m_cond, m_mutex) == 0;
     }
-    bool timewait(pthread_mutex_t *m_mutex, struct timespec t) {
-        int ret = 0;
-        ret = pthread_cond_timedwait(&m_cond, m_mutex, &t);
-        return ret == 0;
+
+    bool timewait(pthread_mutex_t* m_mutex, struct timespec* t) {
+        return pthread_cond_timedwait(&m_cond, m_mutex, t) == 0;
     }
+
     bool signal() {
         return pthread_cond_signal(&m_cond) == 0;
-    }
-    bool broadcast() {
-        return pthread_cond_broadcast(&m_cond) == 0;
     }
 
 private:
@@ -69,32 +66,29 @@ private:
 };
 
 
-// 信号量类
+//信号量类
 class sem {
 public:
-    sem() {
-        if( sem_init( &m_sem, 0, 0 ) != 0 ) {
+    sem(int num = 0) {
+        if(sem_init(&m_sem, 0, num) != 0) {
             throw std::exception();
         }
     }
-    sem(int num) {
-        if( sem_init( &m_sem, 0, num ) != 0 ) {
-            throw std::exception();
-        }
-    }
+
     ~sem() {
-        sem_destroy( &m_sem );
+        sem_destroy(&m_sem);
     }
-    // 等待信号量
+
     bool wait() {
-        return sem_wait( &m_sem ) == 0;
+        return sem_wait(&m_sem) == 0;
     }
-    // 增加信号量
+
     bool post() {
-        return sem_post( &m_sem ) == 0;
+        return sem_post(&m_sem) == 0;
     }
 private:
     sem_t m_sem;
 };
+
 
 #endif
